5925396aee35f36ba2b0a22f12995242b447e1e4
3119005436 <2450854008@qq.com>
2021-09-27 01:10:24 +0800

    添加输入密钥的按钮

--- 第三次作业/第一部分/src/GUI.py
+++ 第三次作业/第一部分/src/GUI.py
@@ -13,43 +13,50 @@ class MY_GUI:
         GUI窗口初始化
         :param init_window: tkinter对象
         """
-        self.init_window_name = init_window
-        self.init_window_name.title("异或加解密")  # 窗口名
+        self.init_window = init_window
+        self.init_window.title("异或加解密")  # 窗口名
         width, height = init_window.maxsize()  # 获取屏幕最大值
-        self.init_window_name.geometry("{}x{}".format(width, height))  # 窗口最大化
+        self.init_window.geometry("{}x{}".format(width, height))  # 窗口最大化
         # 标签
         text_width, text_height = int(width / 8), int(height / 20)
-        self.result_data_label = Label(self.init_window_name, text="文件预览")
+        self.result_data_label = Label(self.init_window, text="文件预览")
         self.result_data_label.grid(row=0)
-        self.log_label = Label(self.init_window_name, text="日志")
+        self.log_label = Label(self.init_window, text="日志")
         self.log_label.grid(row=text_height + 1)
         # 文本框
-        self.preview_Text = Text(self.init_window_name, width=text_width, height=text_height)  # 文件预览结果
+        self.preview_Text = Text(self.init_window, width=text_width, height=text_height)  # 文件预览结果
         self.preview_Text.grid(row=1, column=0, rowspan=text_height)
-        self.log_Text = Text(self.init_window_name, width=text_width, height=10)  # 日志框
+        self.log_Text = Text(self.init_window, width=text_width, height=10)  # 日志框
         self.log_Text.grid(row=text_height + 2, column=0)
         # 滚动条
-        scroll_preview = Scrollbar()
-        scroll_preview.grid(row=4, column=2, sticky='ns')
-        scroll_preview['command'] = self.preview_Text.yview
-        self.preview_Text.config(yscrollcommand=scroll_preview.set)
+        scroll_preview = Scrollbar()    # 实例化滚动条对象
+        scroll_preview.grid(row=6, column=2, sticky='ns')
+        scroll_preview['command'] = self.preview_Text.yview     # 将滚动条与文本框关联
+        self.preview_Text.config(yscrollcommand=scroll_preview.set)     # 将滚动条填充
         scroll_log = Scrollbar()
         scroll_log.grid(row=text_height + 2, column=2, sticky='ns')
         scroll_log['command'] = self.log_Text.yview
         self.log_Text.config(yscrollcommand=scroll_log.set)
         # 按钮
-        self.button_open_file = Button(self.init_window_name, text="打开文件", bg='lightblue', width=10,
+        self.button_open_file = Button(self.init_window, text="打开文件", bg='lightblue', width=10,
                                        command=self.open_file())
         self.button_open_file.grid(row=1, column=text_width)
-        self.button_encrypt = Button(self.init_window_name, text="加密", bg="lightblue", width=10,
+        self.button_key = Button(self.init_window, text="输入密钥", bg='lightblue', width=10,
+                                 command=self.get_key())
+        self.button_key.grid(row=2, column=text_width)
+        self.button_encrypt = Button(self.init_window, text="加密", bg="lightblue", width=10,
                                      command=self.encrypt())
-        self.button_encrypt.grid(row=2, column=text_width)
-        self.button_decrypt = Button(self.init_window_name, text="解密", bg="lightblue", width=10,
+        self.button_encrypt.grid(row=3, column=text_width)
+        self.button_decrypt = Button(self.init_window, text="解密", bg="lightblue", width=10,
                                      command=self.decrypt())
-        self.button_decrypt.grid(row=3, column=text_width)
+        self.button_decrypt.grid(row=4, column=text_width)
 
     def open_file(self):
         """读取文件"""
+        pass
+
+    def get_key(self):
+        """获取密钥"""
         pass
 
     def encrypt(self):
