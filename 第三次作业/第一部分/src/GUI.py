ebaf2b390fb843b8101e823355c5678232927b67
3119005436 <2450854008@qq.com>
2021-09-27 10:58:06 +0800

    修改GUI继承方式，添加输入密钥弹窗类，获取输入密钥

--- 第三次作业/第一部分/src/GUI.py
+++ 第三次作业/第一部分/src/GUI.py
@@ -4,31 +4,55 @@
 __author__ = "Maylon"
 
 from tkinter import *
+from tkinter import filedialog
+import tkinter as tk
 import time
 import os
-from tkinter import filedialog
 
 
-class MY_GUI:
-    def __init__(self, init_window):
+class PopUpDialog(Toplevel):
+    def __init__(self, parent):
+        super().__init__()
+        self.title("输入密钥")
+        self.parent = parent  # 保留父窗口
+        x = parent.winfo_screenwidth() / 2 - 200
+        y = parent.winfo_screenheight() / 2 - 50
+        self.geometry("+%d+%d" % (x, y))        # 设置窗口位置
+        self.text = StringVar()
+        Entry(self, textvariable=self.text, width=60).pack(side='left')     # 文本框居左
+        Button(self, text='确定', command=self.get_text).pack(side='right')   # 按钮居右
+
+    def get_text(self):
+        """
+        获取文本信息
+        :return: None
+        """
+        self.parent.key_words = self.text.get()     # 读取文本框信息
+        self.destroy()      # 销毁当前窗口
+
+
+class MY_GUI(tk.Tk):
+    def __init__(self):
         """
         GUI窗口初始化
-        :param init_window: tkinter对象
         """
-        self.init_window = init_window
-        self.init_window.title("异或加解密")  # 窗口名
-        width, height = init_window.maxsize()  # 获取屏幕最大值
-        self.init_window.geometry("{}x{}".format(width, height))  # 窗口最大化
+        super().__init__()
+        # self.init_window = init_window
+        self.title("异或加解密")  # 窗口名
+        width, height = self.maxsize()  # 获取屏幕最大值
+        self.geometry("{}x{}".format(width, height))  # 窗口最大化
         # 标签
         text_width, text_height = int(width / 8), int(height / 20)
-        self.preview_label = Label(self.init_window, text="文件预览")
+        self.preview_label = Label(self, text="文件预览")
         self.preview_label.grid(row=0)
-        self.log_label = Label(self.init_window, text="日志")
+        self.log_label = Label(self, text="日志")
         self.log_label.grid(row=text_height + 1)
+        # 密钥
+        self.key_words = ""
         # 文本框
-        self.preview_Text = Text(self.init_window, width=text_width, height=text_height)  # 文件预览结果
+        self.preview_Text = Text(self, width=text_width, height=text_height)  # 文件预览结果
         self.preview_Text.grid(row=1, column=0, rowspan=text_height)
-        self.log_Text = Text(self.init_window, width=text_width, height=10)  # 日志框
+        self.log_Text = Text(self, width=text_width, height=10)  # 日志框
         self.log_Text.grid(row=text_height + 2, column=0)
         # 滚动条
         scroll_preview = Scrollbar()  # 实例化滚动条对象
@@ -40,16 +64,16 @@ class MY_GUI:
         scroll_log['command'] = self.log_Text.yview
         self.log_Text.config(yscrollcommand=scroll_log.set)
         # 按钮
-        self.button_open_file = Button(self.init_window, text="打开文件", bg='lightblue', width=10,
+        self.button_open_file = Button(self, text="打开文件", bg='lightblue', width=10,
                                        command=self.open_file)
         self.button_open_file.grid(row=1, column=text_width)
-        self.button_key = Button(self.init_window, text="输入密钥", bg='lightblue', width=10,
+        self.button_key = Button(self, text="输入密钥", bg='lightblue', width=10,
                                  command=self.get_key)
         self.button_key.grid(row=2, column=text_width)
-        self.button_encrypt = Button(self.init_window, text="加密", bg="lightblue", width=10,
+        self.button_encrypt = Button(self, text="加密", bg="lightblue", width=10,
                                      command=self.encrypt)
         self.button_encrypt.grid(row=3, column=text_width)
-        self.button_decrypt = Button(self.init_window, text="解密", bg="lightblue", width=10,
+        self.button_decrypt = Button(self, text="解密", bg="lightblue", width=10,
                                      command=self.decrypt)
         self.button_decrypt.grid(row=4, column=text_width)
 
@@ -58,20 +82,30 @@ class MY_GUI:
         读取文件
         :return: None
         """
-        file = filedialog.askopenfilename(title='选择文件', initialdir=(os.path.expanduser('D:/')))     # 读取D盘文件
-        self.preview_Text.delete('1.0', 'end')      # 清空文本框
+        file = filedialog.askopenfilename(title='选择文件', initialdir=(os.path.expanduser('D:/')))  # 读取D盘文件
+        self.preview_Text.delete('1.0', 'end')  # 清空文本框
         if file is not None:
             try:
-                with open(file, 'rb+') as f:        # 以二进制形式读取文件
+                with open(file, 'rb+') as f:  # 以二进制形式读取文件
                     file_text = f.read()
-                self.preview_Text.insert('insert', file_text)       # 打印读取结果
-                self.write_log_to_Text("Open File: " + str(file))   # 正常记录
+                self.preview_Text.insert('insert', file_text)  # 打印读取结果
+                self.write_log_to_Text("Open File: " + str(file))  # 正常记录
                 f.close()
+            except FileNotFoundError:
+                pass
             except Exception as e:
-                self.write_log_to_Text(e)   # 异常记录
+                self.write_log_to_Text(e)  # 异常记录
 
     def get_key(self):
-        """获取密钥"""
+        """
+        弹出窗口获取密钥
+        :return:
+        """
+        pw = PopUpDialog(self)      # 弹窗
+        self.wait_window(pw)        # 等待
+        return
+
+    def get_text(self):
         pass
 
     def encrypt(self):
@@ -94,9 +128,8 @@ class MY_GUI:
 
 
 def gui_start():
-    init_window = Tk()  # 实例化出一个父窗口
-    MY_GUI(init_window)  # 设置窗口属性
-    init_window.mainloop()  # 保持窗口运行
+    gui = MY_GUI()  # 初始化窗口
+    gui.mainloop()  # 保持窗口运行
 
 
 if __name__ == "__main__":
