cfe0fc32fe070eeffab8b1c5a9837261b83615f6
3119005436 <2450854008@qq.com>
2021-10-08 23:23:29 +0800

    des：string转为bit

---
+++ 第四次作业(DES)/DES.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__author__ = "Maylon"
+
+
+def string2bit(str_data):
+    """
+    将字符串数据转换为比特位列表
+    :param str_data: string
+    :return: list of bits (0,1)'s
+    """
+    data = [ord(c) for c in str_data]
+    L = len(data) * 8
+    result = [0] * L
+    pos = 0
+    for ch in data:
+        i = 7
+        while i >= 0:
+            if ch & (1 << i) != 0:
+                result[pos] = 1
+            else:
+                result[pos] = 0
+            pos += 1
+            i -= 1
+    return result
+
+
+if __name__ == '__main__':
+    bit_res = string2bit(input("input string: "))
+    print(bit_res)
