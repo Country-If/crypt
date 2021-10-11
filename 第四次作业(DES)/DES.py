6c9dcbe6321f31ec7260237c39418d45282ef0b3
3119005436 <2450854008@qq.com>
2021-10-11 20:20:13 +0800

    des：简单封装

--- 第四次作业(DES)/DES.py
+++ 第四次作业(DES)/DES.py
@@ -4,10 +4,10 @@
 __author__ = "Maylon"
 
 # macro definition
-ENCRYPT = 0x00
-DECRYPT = 0x01
-block_size = 64
-key_size = 64
+_ENCRYPT = 0x00
+_DECRYPT = 0x01
+_block_size = 64
+_key_size = 64
 
 
 def string2bit(str_data):
@@ -52,7 +52,7 @@ def bit2string(bit_data):
     # return bytes(result)
 
 
-def permutate(table, block):
+def _permutate(table, block):
     """
     根据指定顺序置换数据
     :param table: permutate list
@@ -62,14 +62,14 @@ def permutate(table, block):
     return list(map(lambda x: block[x], table))
 
 
-def create_sub_keys(key):
+def _create_sub_keys(key):
     """
     创建子密钥
     :param key: bit list
     :return: list of sub_key lists
     """
     # 初始置换
-    key = permutate(pc1, key)
+    key = _permutate(_pc1, key)
     i = 0
     # 切分密钥
     L = key[:28]
@@ -78,14 +78,14 @@ def create_sub_keys(key):
     while i < 16:
         j = 0
         # 根据左移次数列表 循环左移
-        while j < left_rotations[i]:
+        while j < _left_rotations[i]:
             L.append(L[0])
             del L[0]
             R.append(R[0])
             del R[0]
             j += 1
         # 通过pc2 创建子密钥
-        Kn[i] = permutate(pc2, L + R)
+        Kn[i] = _permutate(_pc2, L + R)
         i += 1
     return Kn
 
@@ -99,12 +99,12 @@ def des_crypt(block, Kn, crypt_type):
     :return: bit list
     """
     # 初始置换
-    block = permutate(ip, block)
+    block = _permutate(_ip, block)
     L = block[:32]
     R = block[32:]
 
     # 加密从Kn[1]到Kn[16] (下标从0开始)
-    if crypt_type == ENCRYPT:
+    if crypt_type == _ENCRYPT:
         iteration = 0
         iteration_adjustment = 1
     # 解密从Kn[16]到Kn[1]
@@ -118,7 +118,7 @@ def des_crypt(block, Kn, crypt_type):
         tempR = R[:]
 
         # 选择运算E
-        R = permutate(expansion_table, R)
+        R = _permutate(_expansion_table, R)
 
         # 中间结果与子密钥相异或，切分为8块
         R = list(map(lambda x, y: x ^ y, R, Kn[iteration]))
@@ -135,7 +135,7 @@ def des_crypt(block, Kn, crypt_type):
             n = (B[j][1] << 3) + (B[j][2] << 2) + (B[j][3] << 1) + B[j][4]  # 列号
 
             # 求排列值(S盒的输出值)
-            v = S_box[j][(m << 4) + n]
+            v = _S_box[j][(m << 4) + n]
 
             # 将值转换为位，写入Bn
             Bn[pos] = (v & 8) >> 3
@@ -147,7 +147,7 @@ def des_crypt(block, Kn, crypt_type):
             j += 1
 
         # 置换运算P
-        R = permutate(p, Bn)
+        R = _permutate(_p, Bn)
 
         R = list(map(lambda x, y: x ^ y, R, L))
 
@@ -158,11 +158,11 @@ def des_crypt(block, Kn, crypt_type):
         iteration += iteration_adjustment
 
     # 逆初始置换IP^-1 (R在左 L在右)
-    final = permutate(fp, R + L)
+    final = _permutate(_fp, R + L)
     return final
 
 
-def check_key(key):
+def _check_key(key):
     """
     检查密钥是否符合规范
     :param key: string
@@ -170,13 +170,13 @@ def check_key(key):
     """
     key = string2bit(key)
     key_len = len(key)
-    if key_len % key_size != 0:
-        for i in range(key_size - (key_len % key_size)):
+    if key_len % _key_size != 0:
+        for i in range(_key_size - (key_len % _key_size)):
             key.append(0)  # 不够位数补0
     return key
 
 
-def check_data(data):
+def _check_data(data):
     """
     检查数据是否符合规范
     :param data: string
@@ -185,8 +185,8 @@ def check_data(data):
     data = string2bit(data)
     data_len = len(data)
 
-    if data_len % block_size != 0:
-        for i in range(block_size - (data_len % block_size)):
+    if data_len % _block_size != 0:
+        for i in range(_block_size - (data_len % _block_size)):
             data.append(0)  # 不够位数补0
 
     data_len = len(data)
@@ -194,8 +194,8 @@ def check_data(data):
     i = 0
     # 将数据切块
     while i < data_len - 1:
-        res.append(data[i: i + block_size])
-        i += block_size
+        res.append(data[i: i + _block_size])
+        i += _block_size
     return res
 
 
@@ -206,11 +206,11 @@ def encrypt(data, key):
     :param key: string
     :return: list of bit lists
     """
-    data_list = check_data(data)
-    Kn = create_sub_keys(check_key(key))
+    data_list = _check_data(data)
+    Kn = _create_sub_keys(_check_key(key))
     res = []
     for data in data_list:  # 逐块加密
-        res.append(des_crypt(data, Kn, ENCRYPT))
+        res.append(des_crypt(data, Kn, _ENCRYPT))
     return res
 
 
@@ -221,31 +221,31 @@ def decrypt(block_list, key):
     :param key: string
     :return: list of bit lists
     """
-    Kn = create_sub_keys(check_key(key))
+    Kn = _create_sub_keys(_check_key(key))
     res = []
     for block in block_list:    # 逐块解密
-        res.append(des_crypt(block, Kn, DECRYPT))
+        res.append(des_crypt(block, Kn, _DECRYPT))
     return res
 
 
 # Permutation and translation tables for DES (8 * 7)
-pc1 = [56, 48, 40, 32, 24, 16, 8,
-       0, 57, 49, 41, 33, 25, 17,
-       9, 1, 58, 50, 42, 34, 26,
-       18, 10, 2, 59, 51, 43, 35,
-       62, 54, 46, 38, 30, 22, 14,
-       6, 61, 53, 45, 37, 29, 21,
-       13, 5, 60, 52, 44, 36, 28,
-       20, 12, 4, 27, 19, 11, 3
-       ]
+_pc1 = [56, 48, 40, 32, 24, 16, 8,
+        0, 57, 49, 41, 33, 25, 17,
+        9, 1, 58, 50, 42, 34, 26,
+        18, 10, 2, 59, 51, 43, 35,
+        62, 54, 46, 38, 30, 22, 14,
+        6, 61, 53, 45, 37, 29, 21,
+        13, 5, 60, 52, 44, 36, 28,
+        20, 12, 4, 27, 19, 11, 3
+        ]
 
 # number left rotations of pc1 (1 * 16)
-left_rotations = [
+_left_rotations = [
     1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1
 ]
 
 # permuted choice key (table 2) (8 * 6)
-pc2 = [
+_pc2 = [
     13, 16, 10, 23, 0, 4,
     2, 27, 14, 5, 20, 9,
     22, 18, 11, 3, 25, 7,
@@ -257,18 +257,18 @@ pc2 = [
 ]
 
 # initial permutation IP (8 * 8)
-ip = [57, 49, 41, 33, 25, 17, 9, 1,
-      59, 51, 43, 35, 27, 19, 11, 3,
-      61, 53, 45, 37, 29, 21, 13, 5,
-      63, 55, 47, 39, 31, 23, 15, 7,
-      56, 48, 40, 32, 24, 16, 8, 0,
-      58, 50, 42, 34, 26, 18, 10, 2,
-      60, 52, 44, 36, 28, 20, 12, 4,
-      62, 54, 46, 38, 30, 22, 14, 6
-      ]
+_ip = [57, 49, 41, 33, 25, 17, 9, 1,
+       59, 51, 43, 35, 27, 19, 11, 3,
+       61, 53, 45, 37, 29, 21, 13, 5,
+       63, 55, 47, 39, 31, 23, 15, 7,
+       56, 48, 40, 32, 24, 16, 8, 0,
+       58, 50, 42, 34, 26, 18, 10, 2,
+       60, 52, 44, 36, 28, 20, 12, 4,
+       62, 54, 46, 38, 30, 22, 14, 6
+       ]
 
 # Expansion table for turning 32 bit blocks into 48 bits (8 * 6)
-expansion_table = [
+_expansion_table = [
     31, 0, 1, 2, 3, 4,
     3, 4, 5, 6, 7, 8,
     7, 8, 9, 10, 11, 12,
@@ -280,7 +280,7 @@ expansion_table = [
 ]
 
 # The (in)famous S-boxes [(4 * 16) * 8]
-S_box = [
+_S_box = [
     # S1
     [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
      0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
@@ -331,7 +331,7 @@ S_box = [
 ]
 
 # 32-bit permutation function P used on the output of the S-boxes (6 * 6)
-p = [
+_p = [
     15, 6, 19, 20, 28, 11,
     27, 16, 0, 14, 22, 25,
     4, 17, 30, 9, 1, 7,
@@ -341,7 +341,7 @@ p = [
 ]
 
 # final permutation IP^-1 (8 * 8)
-fp = [
+_fp = [
     39, 7, 47, 15, 55, 23, 63, 31,
     38, 6, 46, 14, 54, 22, 62, 30,
     37, 5, 45, 13, 53, 21, 61, 29,
@@ -357,7 +357,7 @@ if __name__ == '__main__':
     input_key = input('input key: ')
     input_data = input('input data: ')
     print("input data: ")
-    print(check_data(input_data))
+    print(_check_data(input_data))
     encrypt_result = encrypt(input_data, input_key)
     print("encrypt result: ")
     print(encrypt_result)
