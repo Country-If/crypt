fb48de7a930223ec71e795ef0b8cfdb3f4c052f0
3119005436 <2450854008@qq.com>
2021-10-27 22:52:17 +0800

    ELGamal算法：生成大素数p及其本原元g

---
+++ 第五次作业(RSA)/ELGamal.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__author__ = "Maylon"
+
+from BaseFunction import *
+import random
+
+
+def generate_prime(n_digit):
+    """
+    随机生成n位的素数
+
+    :param n_digit: int，素数位数
+    :return: int，n位素数
+    """
+    if n_digit < 1:
+        raise ValueError("素数位数不能小于1")
+    rand_list = random.sample([str(i) for i in range(1, 10)], 9)  # 初始化1-9的不重复列表，防止生成类似999的整数，影响后续操作
+    randint_list = rand_list[: n_digit]
+    if n_digit > 9:  # 超过位数则补齐
+        while len(randint_list) < n_digit:
+            randint_list.append(str(random.randint(0, 9)))
+    randint = int("".join(randint_list))  # 转为整数
+    while not prime_check(randint):  # 得到素数
+        randint += 1
+    return randint
+
+
+def generate_p_g(n_digit):
+    """
+    生成大素数p及其本原元g
+
+    :param n_digit: int，素数位数
+    :return: (p, q), (int, int)
+    """
+    while True:
+        q = generate_prime(n_digit)  # 随机生成一个n位的素数q
+        if prime_check(2 * q + 1):  # 判断 2*q+1 是否为素数
+            p = 2 * q + 1
+            break
+    while True:
+        g = random.randint(2, p - 2)  # 随机选取整数g，g范围：(1, p - 1)
+        if ExpMod(g, 2, p) != 1 and ExpMod(g, q, p) != 1:
+            break
+    return p, g
+
+
+if __name__ == '__main__':
+    n = int(input("input n: "))
+    print(generate_p_g(n))
