a652e17a735a7e2ba764dd33e422004fdf870a0a
3119005436 <2450854008@qq.com>
2021-10-28 00:18:35 +0800

    添加加密算法

--- 第五次作业(RSA)/EIGamal.py
+++ 第五次作业(RSA)/EIGamal.py
@@ -46,6 +46,46 @@ def generate_p_g(n_digit):
     return p, g
 
 
+class EIGamal:
+    def __init__(self, n_digit):
+        self.p, self.g = generate_p_g(n_digit)
+
+    def generate_key(self):
+        """
+        生成双方的公钥和私钥
+
+        :return: (A_private, A_public, B_private, B_public)
+        """
+        A_private = random.randint(2, self.p - 2)  # 随机选择私钥
+        B_private = random.randint(2, self.p - 2)
+        A_public = ExpMod(self.g, A_private, self.p)  # 计算公钥
+        # A_public = pow(self.g, A_private, self.p)
+        B_public = ExpMod(self.g, B_private, self.p)
+        # B_public = pow(self.g, B_private, self.p)
+        return A_private, A_public, B_private, B_public
+
+    def encrypt(self, private_key, public_key, M):
+        """
+        加密算法
+
+        :param private_key: int，自己的私钥
+        :param public_key: int，对方的公钥
+        :param M: int，原始数据
+        :return: (int, int)，密文(C1, C2)
+        """
+        U = ExpMod(public_key, private_key, self.p)  # 由对方的公钥生成
+        C1 = ExpMod(self.g, private_key, self.p)  # 由自己的私钥生成公钥
+        C2 = (U * M) % self.p  # 加密后的内容
+        return C1, C2  # 发送的密文
+
+    def decrypt(self, private_key, public_key, C):
+        pass
+
+
 if __name__ == '__main__':
     n = int(input("input n: "))
-    print(generate_p_g(n))
+    # print(generate_p_g(n))
+    eigamal = EIGamal(n)
+    a_private, a_public, b_private, b_public = eigamal.generate_key()
+    m = int(input("input data: "))
+    print(eigamal.encrypt(a_private, b_public, m))
